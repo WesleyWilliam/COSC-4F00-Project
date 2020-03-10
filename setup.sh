226f8ded95b782a23e182e0ba18f060e49c98798
Brock Jones <bj15nv@brocku.ca>
2020-03-10 12:24:32 -0400

    Removed the downloading libraries from setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,1 @@
-mkdir lib ;
-wget -O lib/rb.tgz "https://redbeanphp.com/downloadredbeanversion.php?f=postgres"
-tar -C lib/ -xvf lib/rb.tgz
-mkdir licenses
-mv lib/licenses.txt licenses/
-rm lib/rb.tgz
-
-
 chmod -R 701 *
