74562ea9817a4b73cf1cfa180240d5fea870eb07
c4f00g05 <c4f00g05@sandcastle.cosc.brocku.ca>
2020-03-14 15:34:04 -0400

    Editor displays component when page loads.

--- view/editor.php
+++ view/editor.php
@@ -21,11 +21,16 @@
     <!-- Javascript code -->
     <script>
 
-      var components = [];
+      var components = ["Website title"];
+      console.log(components);
+      
 
-      $('#editor-user-page').hide();
+      $( document ).ready(function() {
+        showChanges();
+      });
 
-      components
+      // $('#editor-user-page').hide();
+
       // When sidebar item is clicked
       // $('#sidebarList li').on('click', function (e) {
       //   $( "#sidebarList li" ).removeClass("active")
@@ -44,37 +49,14 @@
       // Function to render changes
       function showChanges() {
         $('#editor-user-page').empty()
-        if (components.length == 1) {
-          $('#editor-user-page').removeClass("invisible").addClass("visible");
-        }
+        // if (components.length == 1) {
+        //   $('#editor-user-page').removeClass("invisible").addClass("visible");
+        // }
         for (let i = 0; i < components.length; i++) {
+          console.log(i)
           $('#editor-user-page').append("<h2>" + components[i] + "</h2>")
         }
       }
-	  
-	  //drag and drop stuff
-	  
-function allowDrop(ev) {
-  ev.preventDefault();
-}
-
-function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function drop(ev) {
-  //ev.preventDefault();
-  //var data = ev.dataTransfer.getData("text");
-  //ev.target.appendChild(document.getElementById(data));
-  
-          $('#textModal').modal('show')
-
-  
-}
-	  
-	  
-	  
-	  
 
     </script>
 </head>
@@ -90,7 +72,45 @@ if (!isset($_SESSION)) {
 ?>
 
 <!-- Nav Bar -->
-<?php include 'navbar.php' ?>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">
+  <img src=".\cms_logo.svg" width="30" height="30" alt="">
+  CMS
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Subscriptions
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Premium Plans</a>
+          <a class="dropdown-item" href="#">Standard Plans</a>
+          <a class="dropdown-item" href="#">Free Content</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Domains</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Editor</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
 
 
 
@@ -99,14 +119,13 @@ if (!isset($_SESSION)) {
     <!-- Side bar -->
     <div class="col" id="sidebar">
       <ul class="list-group" id="sidebarList">
-        <li class="list-group-item list-group-item-action" id="text-sidebar-button" data-toggle="modal" data-target="#textModal" draggable="true" ondragstart=drag(event)">
+        <li class="list-group-item list-group-item-action" id="text-sidebar-button" data-toggle="modal" data-target="#textModal">
           <div class="d-flex justify-content-between align-items-center mt-3 mb-3">
           <span>Text</span>
           
           <i data-feather="align-justify"></i>
           </div>
         </li>
-		
         <li class="list-group-item list-group-item-action">
           <div class="d-flex justify-content-between align-items-center mt-3 mb-3">
           <span>Image</span>
@@ -114,7 +133,6 @@ if (!isset($_SESSION)) {
           <i data-feather="image"></i>
           </div>
         </li>
-		
         <li class="list-group-item list-group-item-action">
           <div class="d-flex justify-content-between align-items-center mt-3 mb-3">
           <span>Link</span>
@@ -168,22 +186,7 @@ if (!isset($_SESSION)) {
 
     <!-- Editor -->
     <div class="col-10">
-
-      <div class="d-flex justify-content-between mt-2 mr-4 pb-2 border-bottom"> 
-        <div>
-          <button type="button" class="btn btn-outline-info mr-2">Themes</button>
-          <button type="button" class="btn btn-outline-info mr-2">Help</button>
-          <button type="button" class="btn btn-outline-info">Edit</button>
-        </div>
-        <div>
-          <button type="button" class="btn btn-outline-warning mr-2">Undo</button>
-          <button type="button" class="btn btn-outline-success mr-2">Save</button>
-          <button type="button" class="btn btn-outline-info">Preview</button>
-        </div>
-        
-      </div>
-
-      <div class="jumbotron mt-3 mr-4 visible" id="editor-user-page" ondrop="drop(event)" ondragover="allowDrop(event)">
+      <div class="jumbotron mt-3 mr-3" id="editor-user-page">
       
       </div>
     </div>
