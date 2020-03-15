1fb12a8c60cbb32b72d3a7aaaa3a366b1c70ae7b
c4f00g05 <c4f00g05@sandcastle.cosc.brocku.ca>
2020-03-15 07:53:50 -0400

    Minor changes, navbar & editor

--- view/navbar.php
+++ view/navbar.php
@@ -35,10 +35,6 @@
         <a class="nav-link" href="#">Editor</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
     <a class="btn btn-outline-success my-2 my-sm-0" href="<?php echo $config['home-file-path']; ?>/controller/controller.php?COMMAND=LOGOUT">Logout</a>
   </div>
 </nav>
