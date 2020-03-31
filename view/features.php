b19d547aee88aed8783cc58b7e7e7b3b5f3a7214
c4f00g05 <c4f00g05@sandcastle.cosc.brocku.ca>
2020-03-31 18:40:59 -0400

    feature page

--- view/features.php
+++ view/features.php
@@ -9,17 +9,15 @@
 
     <!-- Including bootstrap CSS files -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
+</body>
     <!--CSS files -->
-    <style>
-        .jumbotron-fluid {
-            background-color: #03a9f4;
-        }
+    <link rel="stylesheet" href="css/features.css">
 
-        #banner-text {
-            color: white;
-        }
-    </style>
 </head>
 
 <body>
@@ -41,6 +39,53 @@
     </div>
 
     <!-- Page Content -->
+    <div class="container" style="padding-bottom:100px;" >
+
+        <div class="container pb-30">            
+            <div class= "container">
+                <img class="d-block w-100" src="img/templates-and-styles-banner.png" alt="templates-and-styles-banner">
+            </div>
+            <hr class="mt-5">
+        </div>
+        <div class="container pb-30">            
+            <div class= "container">
+                <img class="d-block w-100" src="img/drag-banner.png" alt="drag-and-drop">
+            </div>
+            <hr class="mt-5">
+        </div>
+
+        <div class="container pb-30">            
+            <div class= "container">
+                <img class="d-block w-100" src="img/host-free-banner.png" alt="free-web-hosting">
+            </div>
+            <hr class="mt-5">
+        </div>
+
+        <div class="container pb-30">            
+            <div class= "container">
+                <img class="d-block w-100" src="img/web-management-banner.png" alt="multiple-web-management">
+            </div>
+            <hr class="mt-5">
+        </div>
+        
+        <div class="container pb-30">            
+            <div class= "container">
+                <img class="d-block w-100" src="img/plus-more-banner.png" alt="multiple-web-management">
+            </div>
+            <div class= "container text-center">
+                <a class="btn btn-outline-primary btn-lg" href="<?php echo $config['home-file-path'] . '/view/signup.php' ?>">Start Today!</a>
+            </div>
+
+        </div>
+        
+
+
+    </div>
+    
+
+    
+
+    <!-- Page Content -->
 
     <!-- Footer -->
     <footer id="sticky-footer" class="py-4 bg-dark text-white-50 fixed-bottom">
@@ -49,11 +94,6 @@
         </div>
     </footer>
 
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
-</body>
+    
 
 </html>
