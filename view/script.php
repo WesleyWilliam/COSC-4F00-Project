f55bd076825701aa5fa5118f1b976a3119a392c9
Brock Jones <bj15nv@brocku.ca>
2020-04-13 17:04:23 -0400

    Fixed editor broken by indent

--- view/script.php
+++ view/script.php
@@ -1,4 +1,4 @@
-var str = <? php echo json_encode($component); ?>;
+var str = <?php echo json_encode($component); ?>;
 var webpages = JSON.parse(str);
 var currentWebpage = 'homepage';
 var components = webpages[currentWebpage];
