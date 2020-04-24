f5b0af1d377a130568febfeea09ba915924d66d6
Emmanuel Guarino <c4f00g05@sandcastle.cosc.brocku.ca>
2020-04-23 23:54:13 -0400

    bug fix publish button doing weird stuff

--- view/editor.php
+++ view/editor.php
@@ -190,9 +190,9 @@
         <div>
           <button type="button" class="btn btn-outline-success mr-2 save-editor-changes">Save all</button>
           <a role="button" href="<?php echo $config['home-file-path']; ?>/view/publish.php?website=<?php echo $_GET['website'] ?>" target="_blank" class="btn btn-outline-info preview-editor">Preview</a>
-          <a role="button" href="" class="btn btn-outline-danger mr-2" id="publish-button" title="When published anyone can view your website"> 
+          <button type="button" class="btn btn-outline-danger mr-2" id="publish-button" title="When published anyone can view your website"> 
           <?php $res = $model->publishStatus($_GET['website']); if ($res == 'PUBLISHED') {echo 'Unpublish';} elseif ($res == 'UNPUBLISHED') {echo 'Publish';} else {echo $res;}  ?>
-          </a>
+          </button>
 
 
 
